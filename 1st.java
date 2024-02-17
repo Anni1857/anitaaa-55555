743dc3eb39065b4b62ee159643da746781c2775a
Anni1857 <141309687+Anni1857@users.noreply.github.com>
2024-02-17 15:10:09 +0530

    Add files via upload

---
+++ 1st.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class pahli
+{ public static void main( String ar[])
+{ System.out.print("hello world/n");
+ System.out.print("hello world/n");
+  System.out.print("hello world/n");
+}
+}
