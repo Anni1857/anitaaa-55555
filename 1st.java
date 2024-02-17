f337a89d148a9300a9b5157b6540d52e8d762f8e
Anni1857 <141309687+Anni1857@users.noreply.github.com>
2024-02-17 15:11:21 +0530

    Delete 1st.java

--- 1st.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-class pahli
-{ public static void main( String ar[])
-{ System.out.print("hello world/n");
- System.out.print("hello world/n");
-  System.out.print("hello world/n");
-}
-}
